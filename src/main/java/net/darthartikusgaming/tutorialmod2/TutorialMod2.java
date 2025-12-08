57918a0079d052573c6c7d9d1129d6c0b5b01317
DarthArtikus <artus.jentsch@hotmail.com>
2025-12-08 04:14:04 +0100

    Integrated the first two Items

---
+++ src/main/java/net/darthartikusgaming/tutorialmod2/TutorialMod2.java
@@ -0,0 +1,17 @@
+package net.darthartikusgaming.tutorialmod2;
+
+import net.darthartikusgaming.tutorialmod2.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod2 implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod2";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+        ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/darthartikusgaming/tutorialmod2/TutorialMod2Client.java
@@ -0,0 +1,10 @@
+package net.darthartikusgaming.tutorialmod2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialMod2Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
